b0a95260c00c1d31a2868a042278aa4347792a73
Frank Stein <dr.frank.stain@gmail.com>
2016-07-02 23:05:47 +0200

    First comments.

--- include/pc/ProgressCounter.h
+++ include/pc/ProgressCounter.h
@@ -1,6 +1,7 @@
 #pragma once
 
 
+/// @brief	Progress counter.
 namespace pc{};
 
 
@@ -9,6 +10,18 @@ namespace pc{};
 
 namespace pc
 {
+	/**
+		@brief		Progress counter.
+		@note		Use `ProgressCounter::Create()` to easily obtain the instance of counter.
+		@warning	Inherited from `std::enable_shared_from_this`, any allocation out from `std::shared_ptr` considered ill-formed.
+		This counter helps easily control the progress of some tasks.
+		The range of progress is placed in [0.0-1.0] (inclusive) range. This range will be divided between count of goals.
+		Each goal represents the finishing stage of some concurrent or long-running task.
+		The progress goal must be delegated into some task, live while task remains unfinished, and destroyed after task finished.
+		Each progress goal is designed as shared object, so it's pretty easy to hold it inside of task.
+		In same time the tasks operating, the progress counter itself can be polled for the value of current progress.
+		All is thread safe. The polling of counter, the goals creation and completion. Every operation is concurrently safe.
+	*/
 	class ProgressCounter final : public std::enable_shared_from_this<ProgressCounter>
 	{
 	public:
@@ -21,8 +34,8 @@ namespace pc
 			ProcessGoal( const ProgressCounter* counter );
 			~ProcessGoal();
 
-			inline const ProgressCounter* GetCounter() const	{ return m_host_counter.get(); };
-			inline const bool IsValid() const					{ return m_host_counter != nullptr; };
+			inline const ProgressCounter* GetHostCounter() const	{ return m_host_counter.get(); };
+			inline const bool IsValid() const						{ return m_host_counter != nullptr; };
 
 		private:
 			std::shared_ptr<ProgressCounter>	m_host_counter;
@@ -36,7 +49,7 @@ namespace pc
 		inline static auto Create()		{ return std::make_shared<ProgressCounter>(); };
 
 		ProgressCounter() = default;
-		ProgressCounter( const ProgressCounter& other ) = default;
+		ProgressCounter( const ProgressCounter& other ) = delete;
 		ProgressCounter( ProgressCounter&& other ) = delete;
 
 		void Reset();
